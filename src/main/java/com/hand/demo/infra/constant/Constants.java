7e5c0720f0f21dc2a48f9e63b37deca5702f1f7e
Azzam-Hand <muhammad.azzam@hand-global.com>
2024-11-25 17:34:36 +0700

    [ADD] First Commit

--- src/main/java/com/hand/demo/infra/constant/Constants.java
+++ src/main/java/com/hand/demo/infra/constant/Constants.java
@@ -5,6 +5,17 @@ package com.hand.demo.infra.constant;
  */
 public class Constants {
 
+
+    public static final String CODE_RULE = "INV.COUNTING57.COUNT_NUMBER";
+
+    public static class LovCode {
+        public static final String COUNT_STATUS = "INV.COUNTING.COUNT_STATUS";
+        public static final String COUNT_DIMENSION = "INV.COUNTING.COUNT_DIMENSION";
+        public static final String COUNT_TYPE = "INV.COUNTING.COUNT_TYPE";
+
+        public static final String COUNT_MODE = "INV.COUNTING.COUNT_MODE";
+    }
+
     private Constants() {}
 
 
