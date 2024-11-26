5359c3ebf4f95d66c3792ab542ebc97d6a935d5a
unknown <allansugianto@outlook.com>
2024-11-26 11:14:16 +0700

    [ADD] generate DDD for stock

---
+++ src/main/java/com/hand/demo/infra/mapper/InvStockMapper.java
@@ -0,0 +1,23 @@
+package com.hand.demo.infra.mapper;
+
+import io.choerodon.mybatis.common.BaseMapper;
+import com.hand.demo.domain.entity.InvStock;
+
+import java.util.List;
+
+/**
+ * (InvStock)应用服务
+ *
+ * @author Allan
+ * @since 2024-11-26 11:13:35
+ */
+public interface InvStockMapper extends BaseMapper<InvStock> {
+    /**
+     * 基础查询
+     *
+     * @param invStock 查询条件
+     * @return 返回值
+     */
+    List<InvStock> selectList(InvStock invStock);
+}
+
